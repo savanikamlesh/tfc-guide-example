3d854649967dc72814f90288d23bafd4b9cccb8f
savanikamlesh <59213037+savanikamlesh@users.noreply.github.com>
2021-07-03 14:09:31 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "instance_arn" {
   value = aws_instance.ubuntu.arn
 }
 
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
